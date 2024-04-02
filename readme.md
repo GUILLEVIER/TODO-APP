0f061cb5a2fc8b32ec7a544f66f00efc379b1677
guillevier <guillermo.morales.e@usach.cl>
2024-04-02 01:48:45 -0300

    Code format and Test App

--- readme.md
+++ readme.md
@@ -9,4 +9,4 @@ then
 > node server.js
 
 ## Note
-This Web App only works with Chromium-based Browsers:  Google Chrome, The New Microsoft Edge, brave and others.
+This Web App only works with Chromium-based Browsers: Google Chrome, The New Microsoft Edge, brave and others.
--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 /* eslint-disable no-console */
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
-import express, { urlencoded, json } from 'express';
-import path from 'path';
-import favicon from 'serve-favicon';
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
-import { listUserUI, editUserUI } from './app/controllers/user.controller.js';
-import { listTaskUI, editTaskUI } from './app/controllers/task.controller.js';
+import express, { urlencoded, json } from "express";
+import path from "path";
+import favicon from "serve-favicon";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+import { listUserUI, editUserUI } from "./app/controllers/user.controller.js";
+import { listTaskUI, editTaskUI } from "./app/controllers/task.controller.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -17,11 +17,11 @@ const port = process.env.PORT;
 // Create express app
 const app = express();
 
-import v1UserRouter from './app/routes/user.routes.js';
-import v1TaskRouter from './app/routes/task.routes.js';
+import v1UserRouter from "./app/routes/user.routes.js";
+import v1TaskRouter from "./app/routes/task.routes.js";
 
 // Favicon
-app.use(favicon(path.join(__dirname, 'app', 'public', 'img', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, "app", "public", "img", "favicon.ico")));
 
 // Parse requests of content-type - application/x-www-form-urlencoded
 app.use(urlencoded({ extended: true }));
@@ -30,28 +30,30 @@ app.use(urlencoded({ extended: true }));
 app.use(json());
 
 // Settings for Ejs
-app.set('views', path.join(`${__dirname}/app/views`));
-app.set('view engine', 'ejs');
+app.set("views", path.join(`${__dirname}/app/views`));
+app.set("view engine", "ejs");
 
 // Serving static files
-app.use(express.static(__dirname + '/app/public'));
+app.use(express.static(__dirname + "/app/public"));
 
 // Mongoose require
-import { connect } from 'mongoose';
+import { connect } from "mongoose";
 
 // Connecting to the database
 connect(dbUrl, {
   useNewUrlParser: true,
-}).then(() => {
-  console.log('Successfully connected to the database');
-}).catch((err) => {
-  console.log('Could not connect to the database. Exiting now...', err);
-  process.exit();
-});
+})
+  .then(() => {
+    console.log("Successfully connected to the database");
+  })
+  .catch((err) => {
+    console.log("Could not connect to the database. Exiting now...", err);
+    process.exit();
+  });
 
 // Define a simple route
-app.get('/', (req, res) => {
-  res.json({ message: 'Welcome to Todo App.' });
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to Todo App." });
 });
 
 // Require User routes
@@ -59,10 +61,10 @@ app.use("/v1/users", v1UserRouter);
 app.use("/v1/tasks", v1TaskRouter);
 
 // Views for frontend
-app.get('/users-ui/', listUserUI);
-app.get('/users-ui/edit/:userId', editUserUI);
-app.get('/tasks-ui/user/:userId', listTaskUI);
-app.get('/tasks-ui/edit/:taskId', editTaskUI);
+app.get("/users-ui/", listUserUI);
+app.get("/users-ui/edit/:userId", editUserUI);
+app.get("/tasks-ui/user/:userId", listTaskUI);
+app.get("/tasks-ui/edit/:taskId", editTaskUI);
 
 // Listen for requests
 app.listen(port, () => {
